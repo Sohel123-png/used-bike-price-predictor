fa28fd83007ef28cad8638aa66b0c30d11a14b29
Sohel ali <sasalisayyed@gmail.com>
2026-02-25 16:25:03 +0530

    Added deployment configuration

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ def predict():
 
     return render_template('index.html', prediction=result, brands=BRANDS, owners=OWNERS, cities=CITIES)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
